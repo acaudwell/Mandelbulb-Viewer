18e0c6967f4ed858f826049b462157509456a259
Andrew Caudwell <acaudwell@gmail.com>
2010-01-08 17:12:53 +1300

    extended config file class to support multiple sections and values with the same name.

--- src/core/conffile.cpp
+++ src/core/conffile.cpp
@@ -6,9 +6,212 @@ Regex ConfFile_section("^\\s*\\[([^\\]]+)\\]\\s*$");
 // parse key value pair, seperated by an equals sign, removing white space on key and front of the value
 Regex ConfFile_key_value("^\\s*([^=\\s]+)\\s*=\\s*([^\\s].*)?$");
 
-// vec3f, or vec4f with liberal allowance for whitespace
+// vec2f, vec3f, or vec4f with liberal allowance for whitespace
+Regex ConfFile_vec2_value("^\\s*vec2\\(\\s*([0-9.]+)\\s*,\\s*([0-9.]+)\\s*\\)\\s*$");
 Regex ConfFile_vec3_value("^\\s*vec3\\(\\s*([0-9.]+)\\s*,\\s*([0-9.]+)\\s*,\\s*([0-9.]+)\\s*\\)\\s*$");
 Regex ConfFile_vec4_value("^\\s*vec4\\(\\s*([0-9.]+)\\s*,\\s*([0-9.]+)\\s*,\\s*([0-9.]+)\\s*,\\s*([0-9.]+)\\s*\\)\\s*$");
+
+
+//ConfEntry
+
+ConfEntry::ConfEntry(std::string name) {
+    this->name = name;
+}
+
+ConfEntry::ConfEntry(std::string name, std::string value) {
+    this->name  = name;
+    this->value = value;
+}
+
+void ConfEntry::setName(std::string name) {
+    this->name = name;
+}
+
+void ConfEntry::setString(std::string value) {
+    this->value = value;
+}
+
+void ConfEntry::setFloat(float value) {
+    char floattostr[256];
+    sprintf(floattostr, "%.5f", value);
+
+    this->value = std::string(floattostr);
+}
+
+void ConfEntry::setInt(int value) {
+    char inttostr[256];
+    sprintf(inttostr, "%d", value);
+
+    this->value = std::string(inttostr);
+}
+
+void ConfEntry::setVec2(vec2f value) {
+    char vectostr[256];
+    sprintf(vectostr, "vec2(%.5f, %.5f)", value.x, value.y);
+
+    this->value = std::string(vectostr);
+}
+
+void ConfEntry::setVec3(vec3f value) {
+    char vectostr[256];
+    sprintf(vectostr, "vec3(%.5f, %.5f)", value.x, value.y, value.z);
+
+    this->value = std::string(vectostr);
+}
+
+void ConfEntry::setVec4(vec4f value) {
+    char vectostr[256];
+    sprintf(vectostr, "vec4(%.5f, %.5f, %.5f, %.5f)", value.x, value.y, value.z, value.w);
+
+    this->value = std::string(vectostr);
+}
+
+std::string ConfEntry::getString() {
+    return value;
+}
+
+int ConfEntry::getInt() {
+    return atoi(value.c_str());
+}
+
+float ConfEntry::getFloat() {
+    return atof(value.c_str());
+}
+
+bool ConfEntry::getBool() {
+
+    if(value == "1" || value == "yes" || value == "YES" || value == "Yes")
+        return true;
+
+    return false;
+}
+
+vec2f ConfEntry::getVec2() {
+
+    std::vector<std::string> matches;
+
+    if(ConfFile_vec2_value.match(value, &matches)) {
+        return vec2f(atof(matches[0].c_str()), atof(matches[1].c_str()));
+    }
+
+    debugLog("'%s' did not match vec2 regex\n", value.c_str());
+
+    return vec2f(0.0, 0.0);
+}
+
+vec3f ConfEntry::getVec3() {
+
+    std::vector<std::string> matches;
+
+    if(ConfFile_vec3_value.match(value, &matches)) {
+        return vec3f(atof(matches[0].c_str()), atof(matches[1].c_str()), atof(matches[2].c_str()));
+    }
+
+    debugLog("'%s' did not match vec3 regex\n", value.c_str());
+
+    return vec3f(0.0, 0.0, 0.0);
+}
+
+
+vec4f ConfEntry::getVec4() {
+
+    std::vector<std::string> matches;
+
+    if(ConfFile_vec4_value.match(value, &matches)) {
+        return vec4f(atof(matches[0].c_str()), atof(matches[1].c_str()), atof(matches[2].c_str()), atof(matches[3].c_str()) );
+    }
+
+    debugLog("'%s' did not match vec4 regex\n", value.c_str());
+
+    return vec4f(0.0, 0.0, 0.0, 0.0);
+}
+
+//ConfSection
+
+ConfSection::ConfSection() {
+}
+
+ConfSection::ConfSection(std::string name) {
+    this->name = name;
+}
+
+ConfSection::~ConfSection() {
+    clear();
+}
+
+std::string ConfSection::getName() {
+    return name;
+}
+
+ConfEntryList* ConfSection::getEntries(std::string key) {
+    std::map<std::string, ConfEntryList*>::iterator entry_finder = entrymap.find(key);
+
+    if(entry_finder == entrymap.end()) return 0;
+
+    return entry_finder->second;
+}
+
+ConfEntry* ConfSection::getEntry(std::string key) {
+    ConfEntryList* entryList = getEntries(key);
+
+    if(entryList==0 || entryList->size()==0) return 0;
+
+    return entryList->front();
+}
+
+void ConfSection::addEntry(std::string name, std::string value) {
+
+    ConfEntryList* entrylist = entrymap[name];
+
+    if(entrylist==0) {
+        entrymap[name] = entrylist = new ConfEntryList;
+    }
+
+    entrylist->push_back(new ConfEntry(name, value));
+}
+
+//replace first entry with that name
+void ConfSection::setEntry(std::string name, std::string value) {
+    ConfEntryList* entrylist = entrymap[name];
+
+    if(entrylist==0) {
+        entrymap[name] = entrylist = new ConfEntryList;
+    }
+
+    //remove any entries with this name
+    while(entrylist->size()>0) {
+        ConfEntry* front = entrylist->front();
+        entrylist->pop_front();
+        delete front;
+    }
+
+    //add new entry
+    entrylist->push_front(new ConfEntry(name, value));
+}
+
+void ConfSection::clear() {
+
+    //delete entries
+    for(std::map<std::string, ConfEntryList*>::iterator it = entrymap.begin();
+        it!= entrymap.end(); it++) {
+
+        ConfEntryList* entrylist = it->second;
+
+        for(std::list<ConfEntry*>::iterator eit = entrylist->begin();
+            eit != entrylist->end(); eit++) {
+
+            ConfEntry* e = *eit;
+            delete e;
+        }
+
+        delete entrylist;
+    }
+
+    entrymap.clear();
+}
+
+
+//ConfFile
 
 ConfFile::ConfFile() {
 
@@ -22,17 +225,36 @@ void ConfFile::clear() {
 
     conf_error = "";
 
-    //delete stuff from configmap
-    for(std::map<std::string, std::map<std::string, std::string>*>::iterator it = confmap.begin(); it!= confmap.end(); it++) {
-        std::map<std::string, std::string>* sectionmap = it->second;
-        delete sectionmap;
+    //delete sections
+    for(std::map<std::string, ConfSectionList*>::iterator it = sectionmap.begin();
+        it!= sectionmap.end(); it++) {
+
+        ConfSectionList* sectionlist = it->second;
+
+        for(std::list<ConfSection*>::iterator sit = sectionlist->begin();
+            sit != sectionlist->end(); sit++) {
+
+            ConfSection* s = *sit;
+            delete s;
+        }
+
+        delete sectionlist;
     }
 
-    confmap.clear();
+    sectionmap.clear();
 }
 
 std::string ConfFile::getError() {
     return conf_error;
+}
+
+void ConfFile::save(std::string conffile) {
+    this->conffile = conffile;
+    save();
+}
+
+void ConfFile::save() {
+
 }
 
 bool ConfFile::load(std::string conffile) {
@@ -48,7 +270,7 @@ bool ConfFile::load() {
     char buff[1024];
 
     int lineno = 0;
-    std::string section = "";
+    ConfSection* sec = 0;
 
     std::ifstream in(conffile.c_str());
 
@@ -75,7 +297,9 @@ bool ConfFile::load() {
         // sections
         } else if(ConfFile_section.match(line, &matches)) {
 
-            section = matches[0];
+            if(sec != 0) addSection(sec);
+
+            sec = new ConfSection(matches[0]);
 
         // key value pairs
         } else if(ConfFile_key_value.match(line, &matches)) {
@@ -91,16 +315,11 @@ bool ConfFile::load() {
                 else if(string_end != value.size()-1) value = value.substr(0,string_end+1);
             }
 
-            string_to_string_map* sectionmap = confmap[section];
+            if(sec==0) sec = new ConfSection("");
 
-            if(sectionmap == 0) {
-                sectionmap = new std::map<std::string,std::string>();
-                confmap[section] = sectionmap;
-            }
+            sec->addEntry(key, value);
 
-            (*sectionmap)[key] = value;
-
-            debugLog("%s: [%s] %s => %s\n", conffile.c_str(), section.c_str(), key.c_str(), value.c_str());
+            debugLog("%s: [%s] %s => %s\n", conffile.c_str(), sec->getName().c_str(), key.c_str(), value.c_str());
 
         } else {
             sprintf(buff, "failed to read line %d of config file %s", lineno, conffile.c_str());
@@ -108,6 +327,8 @@ bool ConfFile::load() {
             return false;
         }
     }
+
+    if(sec != 0) addSection(sec);
 
     in.close();
 
@@ -124,73 +345,112 @@ bool ConfFile::hasValue(std::string section, std::string key) {
 
 bool ConfFile::hasSection(std::string section) {
 
-    std::map<std::string, string_to_string_map*>::iterator section_finder = confmap.find(section);
+    ConfSection* sec = getSection(section);
 
-    if(section_finder == confmap.end()) return false;
+    if(sec==0) return false;
 
     return true;
 }
 
+void ConfFile::addSection(ConfSection* section) {
+
+    ConfSectionList* sectionlist = getSections(section->getName());
+
+    if(sectionlist==0) {
+        sectionmap[section->getName()] = sectionlist = new ConfSectionList;
+    }
+
+    sectionlist->push_back(section);
+}
+
+//returns the list of all sections with a particular name
+ConfSectionList* ConfFile::getSections(std::string section) {
+    std::map<std::string, ConfSectionList*>::iterator section_finder = sectionmap.find(section);
+
+    if(section_finder == sectionmap.end()) return 0;
+
+    return section_finder->second;
+}
+
+//returns the first section with a particular name
+ConfSection* ConfFile::getSection(std::string section) {
+
+    ConfSectionList* sectionlist = getSections(section);
+
+    if(sectionlist==0 || sectionlist->size()==0) return 0;
+
+    return sectionlist->front();
+}
+
+//returns a list of all entries in a section with a particular name
+ConfEntryList* ConfFile::getEntries(std::string section, std::string key) {
+
+    ConfSection* sec = getSection(section);
+
+    if(sec==0) return 0;
+
+    ConfEntryList* entryList = sec->getEntries(key);
+
+    return entryList;
+}
+
+//get first entry in a section with a particular name
+ConfEntry* ConfFile::getEntry(std::string section, std::string key) {
+
+    ConfSection* sec = getSection(section);
+
+    if(sec==0) return 0;
+
+    ConfEntry* entry = sec->getEntry(key);
+
+    return entry;
+}
+
 std::string ConfFile::getString(std::string section, std::string key) {
 
-    std::map<std::string, string_to_string_map*>::iterator section_finder = confmap.find(section);
+    ConfEntry* entry = getEntry(section, key);
 
-    if(section_finder == confmap.end()) return std::string("");
+    if(entry==0) return std::string("");
 
-    string_to_string_map* sectionmap = section_finder->second;
-
-    std::map<std::string, std::string>::iterator key_finder = sectionmap->find(key);
-
-    if(key_finder == sectionmap->end()) return std::string("");
-
-    return key_finder->second;
+    return entry->getString();
 }
 
 int ConfFile::getInt(std::string section, std::string key) {
-    std::string stringvalue = getString(section, key);
+    ConfEntry* entry = getEntry(section, key);
 
-    return atoi(stringvalue.c_str());
+    if(entry) return entry->getInt();
+
+    return 0;
 }
 
 float ConfFile::getFloat(std::string section, std::string key) {
-    std::string stringvalue = getString(section, key);
+    ConfEntry* entry = getEntry(section, key);
 
-    return atof(stringvalue.c_str());
+    if(entry) return entry->getFloat();
+
+    return 0.0f;
 }
 
 bool ConfFile::getBool(std::string section, std::string key) {
-    std::string stringvalue = getString(section, key);
+    ConfEntry* entry = getEntry(section, key);
 
-    if(stringvalue == "1" || stringvalue == "yes" || stringvalue == "YES" || stringvalue == "Yes")
-        return true;
+    if(entry) return entry->getBool();
 
     return false;
 }
 
 vec3f ConfFile::getVec3(std::string section, std::string key) {
-    std::string stringvalue = getString(section, key);
+    ConfEntry* entry = getEntry(section, key);
 
-    std::vector<std::string> matches;
-
-    if(ConfFile_vec3_value.match(stringvalue, &matches)) {
-        return vec3f(atof(matches[0].c_str()), atof(matches[1].c_str()), atof(matches[2].c_str()));
-    }
-
-    debugLog("'%s' did not match vec3 regex\n", stringvalue.c_str());
+    if(entry) return entry->getVec3();
 
     return vec3f(0.0, 0.0, 0.0);
 }
 
 vec4f ConfFile::getVec4(std::string section, std::string key) {
-    std::string stringvalue = getString(section, key);
+    ConfEntry* entry = getEntry(section, key);
 
-    std::vector<std::string> matches;
+    if(entry) return entry->getVec4();
 
-    if(ConfFile_vec4_value.match(stringvalue, &matches)) {
-        return vec4f(atof(matches[0].c_str()), atof(matches[1].c_str()), atof(matches[2].c_str()), atof(matches[3].c_str()) );
-    }
-
-    debugLog("'%s' did not match vec4 regex\n", stringvalue.c_str());
-
-    return vec4f(1.0, 1.0, 1.0, 1.0);
+    return vec4f(0.0, 0.0, 0.0, 0.0);
 }
--- src/core/conffile.h
+++ src/core/conffile.h
@@ -3,18 +3,67 @@
 
 #include <fstream>
 #include <string>
+#include <list>
 #include <map>
 
 #include "regex.h"
 
-typedef std::map<std::string, std::string> string_to_string_map;
+class ConfEntry {
+    std::string name;
+    std::string value;
+public:
+    ConfEntry();
+    ConfEntry(std::string name);
+    ConfEntry(std::string name, std::string value);
+ 
+    void setName(std::string name);
+
+    void setString(std::string value);
+    void setFloat(float value);
+    void setInt(int value);
+    void setBool(bool value);
+    void setVec2(vec2f value);
+    void setVec3(vec3f value);
+    void setVec4(vec4f value);
+
+    std::string getString();
+    int         getInt();
+    float       getFloat();
+    bool        getBool();
+    vec2f       getVec2();
+    vec3f       getVec3();
+    vec4f       getVec4();
+};
+
+typedef std::list<ConfEntry*> ConfEntryList;
+
+class ConfSection {
+    std::map<std::string, ConfEntryList*> entrymap;
+    std::string name;
+public:
+    ConfSection();
+    ConfSection(std::string name);
+    ~ConfSection();
+
+    void clear();
+
+    ConfEntry* getEntry(std::string key);
+    ConfEntryList* getEntries(std::string key);
+
+    std::string getName();
+
+    void setEntry(std::string name, std::string value);
+    void addEntry(std::string name, std::string value);
+};
+
+typedef std::list<ConfSection*> ConfSectionList;
 
 class ConfFile {
 
     std::string conf_error;
     std::string conffile;
 
-    std::map<std::string, string_to_string_map*> confmap;
+    std::map<std::string, ConfSectionList*> sectionmap;
 public:
     ConfFile();
     ~ConfFile();
@@ -23,7 +72,17 @@ public:
     bool load(std::string conffile);
     bool load();
 
+    void save(std::string conffile);
+    void save();
+
     bool hasSection(std::string section);
+    ConfSection* getSection(std::string section);
+    ConfSectionList* getSections(std::string section);
+
+    ConfEntry*   getEntry(std::string section, std::string key);
+    ConfEntryList* getEntries(std::string section, std::string key);
+
+    void addSection(ConfSection* section);
 
     bool        hasValue(std::string section, std::string key);
     std::string getString(std::string section, std::string key);
@@ -32,7 +91,6 @@ public:
     bool        getBool(std::string section, std::string key);
     vec3f       getVec3(std::string, std::string key);
     vec4f       getVec4(std::string, std::string key);
-
 
     std::string getError();
 };
